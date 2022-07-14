e3717d14b4080c120c1101bf6de9061c739f0fc3
Dong thanh Tran <dtran@f1r5s9.codam.nl>
2022-07-14 16:49:30 +0200

    temp

--- include/fdf.h
+++ include/fdf.h
@@ -1,12 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   fdf.h                                              :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: dtran <dtran@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/07/14 16:47:28 by dtran         #+#    #+#                 */
-/*   Updated: 2022/07/14 16:49:19 by dtran         ########   odam.nl         */
+/*   Created: 2022/07/14 16:43:48 by dtran         #+#    #+#                 */
+/*   Updated: 2022/07/14 16:48:13 by dtran         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FDF_H
+# define FDF_H
+
+# define WIDTH 1920
+# define HEIGHT 1080
+
+#include <stdbool.h>
+#include <errno.h>
+#include <math.h>
+#include "include/MLX42/include/MLX42/MLX42.h"
+
+#endif
