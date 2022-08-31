baed5619bf3db696f5a57078a43676b608089728
Dong thanh Tran <dtran@f1r4s14.codam.nl>
2022-08-31 15:23:41 +0200

    version 1.2

--- src/main.c
+++ src/main.c
@@ -6,12 +6,11 @@
 /*   By: dtran <dtran@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/22 16:43:24 by dtran         #+#    #+#                 */
-/*   Updated: 2022/08/25 19:15:23 by dtran         ########   odam.nl         */
+/*   Updated: 2022/08/31 15:20:43 by dtran         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include <stdio.h>
 
 static int	valid(char *file_name)
 {
@@ -33,28 +32,29 @@ static int	valid(char *file_name)
 
 int	main(int argc, char *argv[])
 {
-	t_data	*data;
+	t_data	data;
 
 	if (argc < 2)
 		ft_error_exit("too few args", 1);
 	if (!valid(argv[1]))
 		return (1);
-	data = (t_data *)malloc(sizeof(t_data));
-	ft_checkmalloc(data);
-	read_file(argv[1], data);
-	data->mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
-	if (!data->mlx)
+	read_file(argv[1], &data);
+	data.mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
+	if (!data.mlx)
+	{
+		free_map_int(&data);
 		ft_error_exit("failed to initialize mlx", 1);
-	data->img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
-	data->scale = 26;
-	data->z_scale = 2;
-	mlx_image_to_window(data->mlx, data->img, 0, 0);
-	fill_bg(data);
-	center_map(data);
-	render_map(data);
-	mlx_key_hook(data->mlx, &key_hook, data);
-	mlx_loop(data->mlx);
-	mlx_delete_image(data->mlx, data->img);
-	mlx_terminate(data->mlx);
+	}
+	data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
+	data.scale = 26;
+	data.z_scale = 2;
+	mlx_image_to_window(data.mlx, data.img, 0, 0);
+	fill_bg(&data);
+	center_map(&data);
+	render_map(&data);
+	mlx_key_hook(data.mlx, &key_hook, &data);
+	mlx_loop(data.mlx);
+	mlx_delete_image(data.mlx, data.img);
+	mlx_terminate(data.mlx);
 	return (0);
 }
